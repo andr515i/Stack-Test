52ceed7e5eab9788f95c79c24afd2db3e24c110a
Andreas Sass Borggaard Madsen <andr515i@zbc.dk>
2022-09-04 16:10:36 +0200

    Add project files.

---
+++ Stack Test/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Test
+{
+    class Book
+    {  // book class, as we dont usually rip pages off or change the contents of the book ,we cant set the properties after it has been created
+        private string _name;
+
+        public string name
+        {
+            get { return _name; }
+        }
+        private int _pageCount;
+
+        public int pageCount
+        {
+            get { return _pageCount; }
+        }
+        private string _bio;
+
+        public string bio
+        {
+            get { return _bio; }
+        }
+
+
+
+        public Book(string name, int pageCount, string bio)
+        {
+            _name = name;
+            _pageCount = pageCount;
+            _bio = bio;
+        }
+    }
+}
---
+++ Stack Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Book> rentableBooks = new Stack<Book>();  //we make a new stack with books ready to be rented by people
+            rentableBooks.Push(new Book("The Art of War", 450, "sun tsu shows us how its done"));
+            rentableBooks.Push(new Book("The 48 Laws of Power", 457, "essential rules for the new machiavellis"));
+            rentableBooks.Push(new Book("numbers", 100000, "every number from 0 to 100.000"));
+
+            Stack<Book> userRentedBooks = new Stack<Book>();  // we assume the user will rent books
+
+
+        }
+    }
+}
